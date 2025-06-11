e0988682d59a3418e05c3309462d5eca951f1b81
ALI SHARHABEEL ALNAJDAWI <125733356+Najdawiali@users.noreply.github.com>
2025-06-11 05:45:01 +0300

    Update TrainingModel.py

--- TrainingModel.py
+++ TrainingModel.py
@@ -1,7 +1,7 @@
 import pickle
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.preprocessing import StandardScaler
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, GRU
